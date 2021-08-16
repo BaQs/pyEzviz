0e5b5478477b27849bd269903bcf302f5cab0559
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2021-08-16 06:47:02 +0200

    Get service urls if login via token.

--- pyezviz/client.py
+++ pyezviz/client.py
@@ -596,6 +596,9 @@ class EzvizClient:
             if not self._token["session_id"]:
                 raise PyEzvizError(f"Relogin required: {req.text}")
 
+            if not self._token["service_urls"]:
+                self._token["service_urls"] = self.get_service_urls()
+
             return self._token
 
         return self._login()

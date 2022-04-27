919cb2ef0e1b07714e78ce8f13befe3cfb3887e6
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2022-04-27 16:00:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyezviz',
-    version="0.2.0.7",
+    version="0.2.0.8",
     license='Apache Software License 2.0',
     author='Pierre Ourdouille',
     author_email='baqs@users.github.com',
